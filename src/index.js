25e7642b1389a72284411d6f3e4a49a9fc33e9fa
O. Sharpe <osharpe001@gmail.com>
2022-11-30 19:51:09 -0500

    online store example

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom"//NECESSARY FOR "ROUTER" FEATURES!!
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
